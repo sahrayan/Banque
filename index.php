17bcb72deb56136e6850fe0841257eaaddc6fdb3
Rayan R <rayanraihani9@gmail.com>
2022-12-09 11:04:59 +0100

    affichage reussi manque le virement bug

--- index.php
+++ index.php
@@ -6,10 +6,11 @@ require_once "Titulaire.php";
 
 
 $titulaire1 = new Titu('RAIHANI ','Rayan ',"27-01-2004 ",'Strasbourg');
-$compte1 =new Compte("Livret b",500,"AED",$titulaire1);
+$compte1 =new Compte("Livret B",500,"AED",$titulaire1);
 $compte2 =new Compte("Livret A",20,"euro",$titulaire1);
 $titulaire1->DispInfo();
-$compte1->Virement($compte2,100)
+
+// $compte1->Virement($compte2,100) 
 
 
 
