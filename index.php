306948b15028b0047a643e52d9f82c3223ea017f
Rayan R <rayanraihani9@gmail.com>
2022-12-09 12:03:39 +0100

    virement//

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $compte1 =new Compte("Livret B",500,"AED",$titulaire1);
 $compte2 =new Compte("Livret A",20,"euro",$titulaire1);
 $titulaire1->DispInfo();
 
-// $compte1->Virement($compte2,100) 
+$compte1->Virement($compte2,10) 
 
 
 
